bd03d491f4711d5fe43e827ca052f2428f34b6ad
jon <jon.p.acampora@gmail.com>
2020-07-22 20:12:37 -0700

    Begin web project structure

---
+++ src/WesternApparel/Controllers/HomepageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WesternApparel.ViewModels;
+
+namespace WesternApparel.Controllers
+{
+    [Route("/")]
+    public class HomepageController : Controller
+    {
+        public ViewResult Index( )
+        {
+            return View( new BaseLayoutViewModel
+            {
+                Title = "Home"
+            } );
+        }
+    }
+}
