596a4c811a05c1afdcb8e314ae96f2f87491622c
jon <jon.p.acampora@gmail.com>
2020-07-22 21:50:53 -0700

    Add basic shop layout

---
+++ src/WesternApparel/Controllers/ShopController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WesternApparel.ViewModels;
+
+namespace WesternApparel.Controllers
+{
+    public class ShopController : Controller
+    {
+        public ViewResult Index( )
+        {
+            return View( new BaseLayoutViewModel { Title = "Shop Western Apparel" } );
+        }
+    }
+}
--- src/WesternApparel/Startup.cs
+++ src/WesternApparel/Startup.cs
@@ -50,7 +50,7 @@ namespace WesternApparel
              {
                  endpoints.MapControllerRoute(
                      name: "default",
-                     pattern: "{controller=Home}/{action=Index}/{id?}" );
+                     pattern: "{controller=Landing}/{action=Index}/{id?}" );
              } );
         }
     }
