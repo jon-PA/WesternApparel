9bc5ed6c5ce5dae283190d09e05ec6d431ab5d33
jon <jon.p.acampora@gmail.com>
2020-07-26 21:26:16 -0700

    Initial product page work

---
+++ src/WesternApparel/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WesternApparel.ViewModels;
+
+namespace WesternApparel.Areas.Shop.Controllers
+{
+    [Route( "shop/[controller]" )]
+    public class ProductController : Controller
+    {
+        [HttpGet("{id}")]
+        public ViewResult ProductView( int id )
+        {
+            return View( new BaseLayoutViewModel { Title = $"Product {id}" } );
+        }
+    }
+}
