a8cdbc22d4624dd87deec4c80c0aa9592bb5c177
jon <jon.p.acampora@gmail.com>
2020-11-26 20:41:21 -0800

    Login "remember me", and login / auth page styling

--- src/WesternApparel/Controllers/AccountController.cs
+++ src/WesternApparel/Controllers/AccountController.cs
@@ -45,7 +45,8 @@ namespace WesternApparel.Controllers
 
                 new AuthenticationProperties
                 {
-                    RedirectUri = redirectUri
+                    RedirectUri = redirectUri,
+                    IsPersistent = viewModel.RetainLogin
                 },
                 CookieAuthenticationDefaults.AuthenticationScheme
             );
